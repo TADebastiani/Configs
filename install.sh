c4305c1c63f1f464d309d9f0504c04c4dc0f8160
Tiago Debastiani <debastianister@gmail.com>
2023-10-25 08:36:15 -0300

    Add configurations for external disk

--- install.sh
+++ install.sh
@@ -9,10 +9,15 @@ function desktop-entries() {
   sh desktop-entries/install.sh
 }
 
+function external-disk() {
+  sh external-disk/install.sh
+ }
+
 if declare -f "$1" > /dev/null
 then
   "$@"
 else
  vim
  desktop-entries
+ external-disk
 fi 
