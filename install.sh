0148f507fe30d755da83460ab7041a403e7d2fdf
Tiago Debastiani <debastianister@gmail.com>
2024-01-26 14:24:04 -0300

    Change main install (in progress)

--- install.sh
+++ install.sh
@@ -1,23 +1,199 @@
 #! /bin/bash
 
+##################################
+# Tiago's Default configurations #
+##################################
 
-function vim() {
-  sh vim/install.sh
+# Variables
+TMPDIR=$(mktemp -d)
+CURRENT_DIR=$(pwd)
+CONFIG_DIR=$CURRENT_DIR
+
+DISK_LABEL=OldHome
+DISK_FORMAT=ext4
+MOUNT_POINT=/mnt/Externo
+UUID_REGEX='[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}'
+
+command_exists() {
+  command -v "$@" > /dev/null 2>&1
 }
 
-function desktop-entries() {
-  sh desktop-entries/install.sh
+configure_external_disk() {
+  fmt_title "Configuring external disks..."
+
+  dependencies=(
+    github-cli
+    vim
+    cmake
+    gcc
+    make
+    python3
+    npm
+    docker
+    jq
+  )
+
+  fmt_message "configuring $DISK_LABEL..."
+  
+  uuid=$(lsblk -f | grep $DISK_LABEL | grep -Eo "$UUID_REGEX")
+
+  if sudo grep -q $uuid /etc/fstab
+  then
+    sudo cp -b /etc/fstab /etc/fstab.bkp
+
+    echo -e "UUID=$uuid $MOUNT_POINT\t$DISK_FORMAT\tdefaults,noatime 0 2" | sudo tee -a /etc/fstab
+  fi
 }
 
-function external-disk() {
-  sh external-disk/install.sh
- }
+configure_keyboard() {
+  fmt_title "Configuring keyboard layout 'English (US, intl., with dead keys)'..."
+  
+  fmt_message "Downloading configuration packages..."
+  wget https://raw.githubusercontent.com/raelgc/win_us_intl/master/.XCompose
+  sudo cp .XCompose $HOME/.XCompose
+  gsettings set org.gnome.settings-daemon.plugins.xsettings disabled-gtk-modules '["'keyboard'"]'
+  yay -S uim
 
-if declare -f "$1" > /dev/null
-then
-  "$@"
-else
- vim
- desktop-entries
- external-disk
-fi 
+  fmt_message "Configuring XProfile..."
+  touch $HOME/.xprofile
+  echo "export GTK_IM_MODULE=uim" >> $HOME/.xprofile
+  echo "export QT_IM_MODULE=uim" >> $HOME/.xprofile
+  echo "uim-xim &" >> $HOME/.xprofile
+  echo "export XMODIFIERS=@im=uim" >> $HOME/.xprofile
+}
+
+ask_menu() {
+  declare -g $1="$2"
+  if [ -z "${!1}" ]
+  then
+    echo -n "$3"
+    read answer
+    if [[ $answer =~ ^(y|Y|s|S)$ ]]
+    then
+      declare -g $1=true
+    else
+      declare -g $1=false
+    fi
+  fi
+}
+
+#menu() {
+  # ask_menu VIM_OPT      "$VIM_OPT"      "Configure Vim? [s/N] "
+  # if $VIM_OPT 
+  # then
+  #   echo "configure_vim"
+  # fi
+  
+  # ask_menu DESKTOP_OPT  "$DESKTOP_OPT"  "Configure Desktop Entries? [s/N] "
+  # if $DESKTOP_OPT
+  # then
+  #   echo "configure_desktop_entries"
+  # fi
+  
+  # ask_menu DOCKER_OPT   "$DOCKER_OPT"   "Configure Docker? [s/N] "
+  # if $DOCKER_OPT
+  # then
+  #   echo "configure_docker"
+  # fi
+  
+  # ask_menu DISKS_OPT    "$DISKS_OPT"    "Configure External Disks? [s/N] "
+  # if $DISKS_OPT
+  # then
+  #   echo "configure_external_disk"
+  # fi
+#}
+
+main() {
+    source ./utils.sh
+  cd $TMPDIR
+
+  setup_colors
+
+  # Install options
+  ALL_OPT=0
+  VIM_OPT=0
+  NVIM_OPT=0
+  DESKTOP_OPT=0
+  DOCKER_OPT=0
+  DISKS_OPT=0
+  KEYBOARD_OPT=0
+
+  # Flag to not ask if some option was passed
+  NO_MENU=0
+
+  while [[ ! -z "$1" ]]
+  do
+      case "$1" in
+          -a|--all) 
+              ALL_OPT=1
+              NO_MENU=1
+              ;;
+          -v|--vim) 
+              VIM_OPT=1
+              NO_MENU=1
+              ;;
+          -n|--nvim)
+              NVIM_OPT=1
+              NO_MENU=1
+              ;;
+          -D|--desktop-entries) 
+              DESKTOP_OPT=1
+              NO_MENU=1
+              ;;
+          -d|--docker)
+              DOCKER_OPT=1
+              NO_MENU=1
+              ;;
+          -e|--external-disks)
+              DISKS_OPT=1
+              NO_MENU=1
+              ;;
+          -k|--keyboard)
+              KEYBOARD_OPT=1
+              NO_MENU=1
+              ;;
+          -h|--help) echo "USAGE" ;;
+      esac
+      shift
+  done
+  
+ # if [[ ! -z "$NO_MENU" ]]
+ # then
+ #   menu
+ # fi
+
+  if [ $NVIM_OPT -eq 1 ]
+  then
+      source $CONFIG_REPO_DIR/nvim/install.sh
+      configure_nvim
+  fi
+
+  if [ $VIM_OPT -eq 1 ]
+  then
+      source $CONFIG_DIR/vim/install.sh
+      configure_vim
+  fi
+  
+  if [ $DESKTOP_OPT -eq 1 ]
+  then
+      source $CONFIG_DIR/desktop-entries/install.sh
+      configure_desktop_entries
+  fi
+  
+  if [ $DOCKER_OPT -eq 1 ]
+  then
+      source $CONFIG_DIR/docker/install.sh
+      configure_docker
+  fi
+  
+  # if $DISKS_OPT
+  # then
+  #   echo "configure_external_disk"
+  # fi
+
+  cd $CURRENT_DIR  
+  rm -r $TMPDIR
+
+}
+
+main $@
---
+++ utils.sh
@@ -0,0 +1,149 @@
+#! /bin/bash
+
+##################################
+# Tiago's default configurations #
+#                                #
+# Utils methods                  #
+##################################
+
+
+# COPY FROM OH-MY-ZSH INSTALL.SH
+# The [ -t 1 ] check only works when the function is not called from
+# a subshell (like in `$(...)` or `(...)`, so this hack redefines the
+# function at the top level to always return false when stdout is not
+# a tty.
+if [ -t 1 ]; then
+  is_tty() {
+    true
+  }
+else
+  is_tty() {
+    true
+  }
+fi
+
+# COPY FROM OH-MY-ZSH INSTALL.SH
+supports_truecolor() {
+    case "$COLORTERM" in
+        truecolor|24bit) return 0 ;;
+    esac
+
+    case "$TERM" in
+        iterm           |\
+        tmux-truecolor  |\
+        linux-truecolor |\
+        xterm-truecolor |\
+        screen-truecolor) return 0 ;;
+    esac
+
+    return 1
+}
+
+fmt_link() {
+    # $1: text, $2: url, $3: fallback mode
+    if supports_hyperlinks; then
+        printf '\033]8;;%s\033\\%s\033]8;;\033\\\n' "$2" "$1"
+        return
+    fi
+
+    case "$3" in
+        --text) printf '%s\n' "$1" ;;
+        --url|*) fmt_underline "$2" ;;
+    esac
+}
+
+fmt_underline() {
+  is_tty && printf '\033[4m%s\033[24m\n' "$*" || printf '%s\n' "$*"
+}
+
+# shellcheck disable=SC2016 # backtick in single-quote
+fmt_code() {
+  is_tty && printf '`\033[2m%s\033[22m`\n' "$*" || printf '`%s`\n' "$*"
+}
+
+fmt_error() {
+  printf '%sError: %s%s\n' "${FMT_BOLD}${FMT_RED}" "$*" "$FMT_RESET" >&2
+}
+
+fmt_message() {
+  echo "${FMT_BLUE}-${FMT_RESET} $*"
+}
+
+fmt_title() {
+  echo "${FMT_BOLD}${FMT_BLUE}::${FMT_RESET} $*${FMT_RESET}"
+}
+
+fmt_question() {
+  echo -n "${FMT_BOLD}${FMT_BLUE}::${FMT_RESET} $*${FMT_RESET}"
+}
+
+
+setup_colors() {
+  # Only use colors if connected to a terminal
+  if ! is_tty; then
+    FMT_RAINBOW=""
+    FMT_RED=""
+    FMT_GREEN=""
+    FMT_YELLOW=""
+    FMT_BLUE=""
+    FMT_BOLD=""
+    FMT_RESET=""
+    return
+  fi
+
+  if supports_truecolor; then
+    FMT_RAINBOW="
+      $(printf '\033[38;2;255;0;0m')
+      $(printf '\033[38;2;255;97;0m')
+      $(printf '\033[38;2;247;255;0m')
+      $(printf '\033[38;2;0;255;30m')
+      $(printf '\033[38;2;77;0;255m')
+      $(printf '\033[38;2;168;0;255m')
+      $(printf '\033[38;2;245;0;172m')
+    "
+  else
+    FMT_RAINBOW="
+      $(printf '\033[38;5;196m')
+      $(printf '\033[38;5;202m')
+      $(printf '\033[38;5;226m')
+      $(printf '\033[38;5;082m')
+      $(printf '\033[38;5;021m')
+      $(printf '\033[38;5;093m')
+      $(printf '\033[38;5;163m')
+    "
+  fi
+  FMT_RED=$(printf '\033[31m')
+  FMT_GREEN=$(printf '\033[32m')
+  FMT_YELLOW=$(printf '\033[33m')
+  FMT_BLUE=$(printf '\033[34m')
+  FMT_BOLD=$(printf '\033[1m')
+  FMT_RESET=$(printf '\033[0m')
+}
+
+is_installed() {
+  if yay -Qis $1 > /dev/null;
+  then
+    return 0
+  fi
+  return 1
+}
+
+install_dependencies() {
+  fmt_title "Installing dependencies..."
+  
+  local packages="$1"
+  local toInstall=()
+
+  for pkg in ${packages[@]}
+  do
+    if ! is_installed $pkg;
+    then
+      toInstall+=($pkg)
+    fi
+  done
+
+  if [[ ${toInstall[@]} != "" ]]
+  then
+    yay -Sy --noconfirm ${toInstall[@]} 
+  fi
+}
