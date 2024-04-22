e4f1858116958343d54a5e7ea4ddb7fa9805877e
Tiago Debastiani <tiago.debastiani@compasso.com.br>
2024-04-22 19:23:35 -0300

    change some plugins

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,42 +1,47 @@
 local ensure_installed = {
-	"lua_ls",
-	"tsserver",
+    "lua_ls",
+    "tsserver",
 }
 
 return {
-	{
-		"williamboman/mason.nvim",
-		lazy = false,
-		priority = 100,
-		config = function()
-			require("mason").setup()
-		end,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		lazy = false,
-		config = function()
-			local lspconfig = require("lspconfig")
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+    {
+        "williamboman/mason.nvim",
+        lazy = false,
+        priority = 100,
+        config = function()
+            require("mason").setup()
+        end,
+    },
+    {
+        "williamboman/mason-lspconfig.nvim",
+        lazy = false,
+        config = function()
+            local lspconfig = require("lspconfig")
+            local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-			require("mason-lspconfig").setup({
-				ensure_installed,
-				handlers = {
-					function(server)
-						lspconfig[server].setup({
-							capabilities = capabilities,
-						})
-					end,
-				},
-			})
-		end,
-	},
-	{
-		"neovim/nvim-lspconfig",
-		lazy = false,
-		keys = {
-			{ "K", vim.lsp.buf.hover },
-			{ "<leader>ca", vim.lsp.buf.code_action, { "n", "v" } },
-		},
-	},
+            require("mason-lspconfig").setup({
+                ensure_installed,
+                handlers = {
+                    function(server)
+                        lspconfig[server].setup({
+                            capabilities = capabilities,
+                        })
+                    end,
+                },
+            })
+        end,
+    },
+    {
+        "neovim/nvim-lspconfig",
+        lazy = false,
+        keys = {
+            { "K",          vim.lsp.buf.hover },
+            { "<leader>ca", vim.lsp.buf.code_action,   { "n", "v" } },
+            { "<leader>gd", vim.lsp.buf.definition },
+            { "<leader>gD", vim.lsp.buf.declaration },
+            { "<leader>gi", vim.lsp.buf.implementation },
+            { "<leader>rn", vim.lsp.buf.rename },
+            { "<leader>e",  vim.diagnostic.open_float }
+        },
+    },
 }
--- nvim/lua/plugins/obsidian.lua
+++ nvim/lua/plugins/obsidian.lua
@@ -1,35 +1,48 @@
 return {
-	"epwalsh/obsidian.nvim",
-  enable = false,
-	dependencies = {
-		-- Required
-		"nvim-lua/plenary.nvim",
-		"hrsh7th/nvim-cmp",
-		"nvim-telescope/telescope.nvim",
-		"nvim-treesitter/nvim-treesitter",
-	},
-	opts = {
-		workspaces = {
-			{
-				name = "default",
-				path = "~/Documents/Obsidian",
-			},
-		},
-		log_level = vim.log.levels.INFO,
-		completion = {
-			nvim_cmp = true,
-			min_chars = 3,
-		},
-		picker = {
-			name = "telescope.nvim",
-		},
-		note_id_func = function(title)
-			return title
-		end,
-	},
-	keys = {
-		{ "<leader>oo", "<cmd>ObsidianOpen<CR>", desc = "Open file in Obsidian" },
-		{ "<leader>os", "<cmd>ObsidianSearch<CR>" },
-		{ "<leader>of", "<cmd>ObsidianFollowLink<CR>" },
-	},
+    "epwalsh/obsidian.nvim",
+    enable = true,
+    lazy = false,
+    dependencies = {
+        -- Required
+        "nvim-lua/plenary.nvim",
+        "hrsh7th/nvim-cmp",
+        "nvim-telescope/telescope.nvim",
+        "nvim-treesitter/nvim-treesitter",
+    },
+    opts = {
+        workspaces = {
+            {
+                name = "personal",
+                path = "~/Documentos/Obsidian/Vaults/personal",
+            },
+            {
+                name = "compass",
+                path = "~/Documentos/Obsidian/Vaults/compass",
+            },
+            {
+                name = "vivo",
+                path = "~/Documentos/Obsidian/Vaults/vivo",
+            }
+        },
+        log_level = vim.log.levels.INFO,
+        completion = {
+            nvim_cmp = true,
+            min_chars = 3,
+        },
+        picker = {
+            name = "telescope.nvim",
+        },
+        note_id_func = function(title)
+            return title
+        end,
+    },
+    keys = {
+        { "<leader>oo", "<cmd>ObsidianOpen<CR>",      desc = "Open file in Obsidian" },
+        { "<leader>os", "<cmd>ObsidianSearch<CR>" },
+        { "<leader>of", "<cmd>ObsidianFollowLink<CR>" },
+    },
+    config = function(_, opts)
+        require('obsidian').setup(opts)
+        vim.opt.conceallevel = 1
+    end
 }
