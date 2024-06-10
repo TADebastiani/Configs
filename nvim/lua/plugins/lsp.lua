d16c247c5a2c12b6539a24c1eb06bad1f431e94d
Tiago Debastiani <debastianister@gmail.com>
2024-06-10 15:27:35 -0300

    Add palantir java formatting

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -4,6 +4,7 @@ local ensure_installed = {
     "stylua",
     "lua_ls",
     "tsserver",
+    "jdtls",
 }
 
 return {
@@ -13,22 +14,47 @@ return {
         priority = 100,
         config = function()
             require("mason").setup()
+            -- require("mason").setup({
+            --     registries = {
+            --         --"github:mason-org/mason-registry",
+            --         "file:~/.config/nvim/lua/settings/mason-registry",
+            --     },
+            -- })
         end,
     },
     {
         "williamboman/mason-lspconfig.nvim",
+        dependencies = {
+            "mfussenegger/nvim-jdtls",
+        },
         lazy = false,
         config = function()
             local lspconfig = require("lspconfig")
-            local capabilities = require("cmp_nvim_lsp").default_capabilities()
+            local cmp_lsp = require("cmp_nvim_lsp")
+            local jdtls_settings = require("settings.lsp.jdtls")
 
             require("mason-lspconfig").setup({
                 ensure_installed,
+                automatic_installation = true,
                 handlers = {
                     function(server)
-                        lspconfig[server].setup({
-                            capabilities = capabilities,
-                        })
+                        local capabilities = vim.lsp.protocol.make_client_capabilities()
+                        capabilities.textDocument.completion.completionItem.snippetSupport = true
+
+                        local config = {
+                            capabilities = cmp_lsp.default_capabilities(capabilities),
+                        }
+
+                        if server == "jdtls" then
+                            config = vim.tbl_deep_extend("keep", config, jdtls_settings.get_config())
+                        else
+                            config.on_attach = function(client)
+                                vim.print(client)
+                                vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+                            end
+                        end
+
+                        lspconfig[server].setup(config)
                     end,
                 },
             })
@@ -38,13 +64,13 @@ return {
         "neovim/nvim-lspconfig",
         lazy = false,
         keys = {
-            { "K",          vim.lsp.buf.hover },
-            { "<leader>ca", vim.lsp.buf.code_action,   { "n", "v" } },
+            { "K", vim.lsp.buf.hover },
+            { "<leader>ca", vim.lsp.buf.code_action, { "n", "v" } },
             { "<leader>gd", vim.lsp.buf.definition },
             { "<leader>gD", vim.lsp.buf.declaration },
             { "<leader>gi", vim.lsp.buf.implementation },
             { "<leader>rn", vim.lsp.buf.rename },
-            { "<leader>e",  vim.diagnostic.open_float },
+            { "<leader>e", vim.diagnostic.open_float },
         },
     },
 }
--- nvim/lua/plugins/none-ls.lua
+++ nvim/lua/plugins/none-ls.lua
@@ -1,30 +1,23 @@
 return {
     "nvimtools/none-ls.nvim",
     config = function()
-        -- local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
-
         local null_ls = require("null-ls")
+        local custom_formatting = require("settings.formatting")
         null_ls.setup({
+            debug = true,
             sources = {
                 null_ls.builtins.formatting.stylua,
                 -- Javascript/Typescript
                 --null_ls.builtins.diagnostics.eslint,
                 null_ls.builtins.formatting.prettier,
-            }
-            -- on_attach = function(client, bufnr)
-            -- 	if client.supports_method("textDocument/formatting") then
-            -- 		vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-            -- 		vim.api.nvim_create_autocmd("BufWritePre", {
-            -- 			group = augroup,
-            -- 			buffer = bufnr,
-            -- 			callback = function()
-            -- 				vim.lsp.buf.format({ async = false })
-            -- 			end,
-            -- 		})
-            -- 	end
-            -- end,
+                custom_formatting.palantir,
+            },
+            on_attach = function(client)
+                if client.supports_method("textDocument/formatting") then
+                    vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+                end
+            end,
         })
 
-        vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
     end,
 }
